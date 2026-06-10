b0d7628e1d6e71b7302f916b5ee20960a5d27bd0
itzluthfi <luthfishidqi2@gmail.com>
2026-06-10 11:39:13 +0700

    up

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,9 +30,11 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = $this->faker ?? \Faker\Factory::create();
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
